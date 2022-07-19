3e82521d4633c833adec573f26b6426b0443146d
Sam Barnard <sam@DESKTOP-KK2KJEE.localdomain>
2022-07-19 19:22:17 +0100

    Rendering out some basic data returned from the weather api.

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,22 @@
 <script setup lang="ts">
 import { ref } from 'vue'
 import { useWeatherApi } from './composables/weather-api'
+import dayjs from 'dayjs'
 
-const query = ref('')
+interface WeatherApiResponse {
+
+  // In production these should *not* really be 'any' but this public api is massive
+  current: any;
+  location: any;
+}
+
+const query = ref<string>('')
+const weather = ref<WeatherApiResponse | null>(null)
 
 async function getWeather() {
   const { getWeatherUsingQuery } = useWeatherApi()
   const { data } = await getWeatherUsingQuery(query.value)
-  console.log('data', data)
+  weather.value = data
 }
 </script>
 
@@ -17,7 +26,7 @@ async function getWeather() {
       <h1 class="text-5xl font-bold">Weather Report</h1>
       <p>A weather reporting tool that utilises the WeatherAPI and is built with Vite + Vue 3 + TypeScript & Tailwind.</p>
     </div>
-    <div class="container mx-auto px-2 space-y-2">
+    <div class="container mx-auto px-2 space-y-2 mb-12">
       <p class="text-2xl">"What's the weather like then?"</p>
       <div class="flex items-center space-x-4">
         <div>
@@ -41,6 +50,41 @@ async function getWeather() {
             GO
           </button>
         </form>
+      </div>
+    </div>
+    <div
+      v-if="weather"
+      class="container mx-auto px-2 space-y-2"
+    >
+      <div>
+        <div class="text-left inline-block border border-white rounded-xl bg-slate-900 text-white p-4">
+          <div class="flex items-center space-x-2">
+            <img
+              :src="weather.current.condition.icon"
+            />
+            <div>
+              <h2 class="text-2xl leading-tight">
+                {{ weather.current.condition.text }}
+              </h2>
+              <h2 class="text-4xl font-bold leading-tight">
+                {{ weather.current.temp_c }}<span class="text-xl font-normal">&deg;c</span> / {{ weather.current.temp_f }}<span class="text-xl font-normal">&deg;f</span>
+              </h2>
+              <div>
+                <h2 class="text-2xl">
+                  {{ weather.location.name }}, {{ weather.location.country }}
+                </h2>
+                <h3>
+                  <span class="text-lg align-top">
+                    at
+                  </span>
+                  <span class="text-4xl font-bold text-orange-300">
+                    {{ dayjs(weather.current.last_updated).format('HH:mm') }}
+                  </span>
+                </h3>
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
